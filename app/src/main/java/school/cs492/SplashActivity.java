c5c2127328bd4330437f0b3408d47f7517c2dc68
wul02 <wul02@students.ipfw.edu>
2015-03-02 21:05:36 -0500

    add splaaaaaaaaaassssshhhhhhh screen to app

--- app/src/main/java/school/cs492/SplashActivity.java
+++ app/src/main/java/school/cs492/SplashActivity.java
@@ -1,39 +1,46 @@
 package school.cs492;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.WindowManager;
 
-
+/**
+ * Created by lingxi on 2/21/2015.
+ */
 public class SplashActivity extends Activity {
-
+    private static long SLEEP_TIME=5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //disable title bar and notification bar.
+        //   this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        //after 5 seconds, launch main activity.
+        IntentLauncher launcher = new IntentLauncher();
+        launcher.start();
+
     }
 
+    private class IntentLauncher extends Thread{
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
+        public void run(){
+            try{
+                //sleeping...
+                Thread.sleep(SLEEP_TIME*1000);
+            }catch(Exception e){
+                Log.e("Splash class", e.getMessage());
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            //start main activity...
+            Intent intent = new Intent(SplashActivity.this, ConfirmationActivity.class);
+            SplashActivity.this.startActivity(intent);
+            SplashActivity.this.finish();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
