5ee7091e505cbaf7c4865bd50fbbe57a49fd543f
wul02 <wul02@students.ipfw.edu>
2015-03-02 21:07:20 -0500

    add splaaaaaaaaaassssshhhhhhh screen to app

--- app/src/main/java/school/cs492/SplashActivity.java
+++ app/src/main/java/school/cs492/SplashActivity.java
@@ -16,7 +16,6 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         //disable title bar and notification bar.
-        //   this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash);
