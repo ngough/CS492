c6b268a261af378604f0b3f75d0fce57eb0be0f7
wul02 <wul02@students.ipfw.edu>
2015-03-22 20:46:39 -0400

    QR scanner is implemented..
    1. Added library ZBar to libs.
    2. Modified dependency to compile the library with project solution.
    3. Re-built.
    4. The Scanned result will be printed in the MainMenuActivity right above "Start Scan" button (Just for the testing purpose)

--- app/src/main/java/school/cs492/MainMenuActivity.java
+++ app/src/main/java/school/cs492/MainMenuActivity.java
@@ -1,17 +1,57 @@
 package school.cs492;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainMenuActivity extends ActionBarActivity {
+
+    private int SCAN_REQUEST_CODE = 1000; // Request code to scan.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        // Get the scan button. Id id button. the other one is button2.
+        // Hook it up with listener.
+        Button scanBtn = (Button)findViewById(R.id.button);
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleBtn((Button) v);
+            }
+        });
+    }
+
+    // This is the listener for the button. It calls CameraScanActivity which is a QR reader.
+    public void handleBtn(Button btn)
+    {
+        Intent intent = new Intent(this, CameraScanActivity.class);
+        startActivityForResult(intent,SCAN_REQUEST_CODE );
+    }
+
+    // Get the QR Result from CameraScanActivity. It calls handleQrResult to do the actual work.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if(requestCode == SCAN_REQUEST_CODE){
+            handleQrResult(resultCode, intent);
+        }
+    }
+
+    // Print QR info on the textView.
+    public void handleQrResult(int resultCode, Intent intent)
+    {
+        if(resultCode==RESULT_OK){
+            TextView infoView = (TextView) findViewById(R.id.infoView);
+            String qrResult = intent.getStringExtra(CameraScanActivity.QR_RESULT_EXTRA);
+            infoView.setText(qrResult);
+        }
     }
 
 
